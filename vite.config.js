256e7d45e71ca90fc41bfc4b7d96b3a3ce6664b4
Maylse <151539643+Maylse@users.noreply.github.com>
2025-04-21 23:43:44 +0800

    upload 4

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/WeatherSafe-Web-/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
+  publicDir: "public", // Ensure this is set
   server: {
     proxy: {
       "/api": {
