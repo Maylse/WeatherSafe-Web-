27b260fb0f3efeb0c0fde85c9c239bad631480d9
Maylse <151539643+Maylse@users.noreply.github.com>
2025-04-21 23:11:56 +0800

    upload web

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/WeatherSafe-Web-/",
   plugins: [react()],
   server: {
     proxy: {
